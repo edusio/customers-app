83539d5afd346a1e9ef90a5592fa3a9d2b193cd3
Eduardo López <edu.1990@hotmail.com>
2020-01-15 10:25:37 +0100

    custom Field and validator

--- src/components/CustomerEdit.js
+++ src/components/CustomerEdit.js
@@ -3,6 +3,17 @@ import PropTypes from 'prop-types';
 import { reduxForm, Field } from 'redux-form';
 import { setPropsAsInitial } from '../helpers/setPropsAsInitial';
 
+const isRequired = value =>(
+    !value && " Este campo es requerido"
+);
+
+const MyField = ({ input, meta}) => (
+    <div>
+        <input {...input} type="text" />
+        { meta.touched && meta.error && <span>{ meta.error }</span> }
+    </div>
+);
+
 const CustomerEdit = ({name, dni, age}) => {
     return (
         <div>
@@ -10,11 +21,21 @@ const CustomerEdit = ({name, dni, age}) => {
             <form action="">
                 <div>
                     <label htmlFor="name">Name </label>
-                    <Field name="name" component="input" type="text"></Field>
+                    <Field
+                        name="name"
+                        component= {MyField}
+                        type="text"
+                        validate={isRequired}
+                    />
                 </div>
                 <div>
                     <label htmlFor="dni">Dni </label>
-                    <Field name="dni" component="input" type="text"></Field>
+                    <Field
+                        name="dni"
+                        component= {MyField}
+                        type="text"
+                        validate={isRequired}
+                    />
                 </div>
                 <div>
                     <label htmlFor="age">Edad </label>
