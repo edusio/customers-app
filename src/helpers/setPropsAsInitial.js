b3f11e5717689af4df3ed9531bf71a6eef73f51e
Eduardo López <edu.1990@hotmail.com>
2020-01-15 10:06:55 +0100

    create helper for initialProps (hight order component)

---
+++ src/helpers/setPropsAsInitial.js
@@ -0,0 +1,11 @@
+
+import React, { Component } from "react";
+
+//Hight order component
+export const setPropsAsInitial = WrappedComponent => (
+    class extends Component {
+        render() {
+            return <WrappedComponent { ...this.props } initialValues={ this.props }/>
+        }
+    }
+);
