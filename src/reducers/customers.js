4a2402ef3c38053f7b03411d7ed5e5d3a3b5048c
Eduardo López <edu.1990@hotmail.com>
2020-01-16 14:10:07 +0100

    refactor edit customer action, now no fetch the customers

--- src/reducers/customers.js
+++ src/reducers/customers.js
@@ -1,8 +1,24 @@
 import { handleActions } from 'redux-actions';
-import { FETCH_CUSTOMERS, CREATE_CUSTOMER } from '../constants';
+import { FETCH_CUSTOMERS, CREATE_CUSTOMER, UPDATE_CUSTOMERS } from '../constants';
 
 // TODO se pueden poner mas de una actions
 export const customers = handleActions({
   [FETCH_CUSTOMERS]: (state, action) => [...action.payload],
   [CREATE_CUSTOMER]: (state, action) => [...state, action.payload],
+  [UPDATE_CUSTOMERS]: (state, action) => {
+    const customerPayload = action.payload;
+    const id = customerPayload.id;
+    const customers = state;
+    const initialvalue = [];
+    const newCustomers = customers.reduce((acumulado, customer) => {
+      if(customer.id === id) {
+          return [...acumulado, customerPayload];
+      }
+      else {
+        return [...acumulado, customer];
+      }
+    }, initialvalue);
+
+    return newCustomers;
+  }
 }, []);
