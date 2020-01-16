133ca1a7372400c02b06a59a1bebaee934532674
Eduardo López <edu.1990@hotmail.com>
2020-01-16 14:37:02 +0100

    refactor

--- src/reducers/customers.js
+++ src/reducers/customers.js
@@ -6,10 +6,12 @@ export const customers = handleActions({
   [FETCH_CUSTOMERS]: (state, action) => [...action.payload],
   [CREATE_CUSTOMER]: (state, action) => [...state, action.payload],
   [UPDATE_CUSTOMERS]: (state, action) => {
+
     const customerPayload = action.payload;
     const id = customerPayload.id;
     const customers = state;
     const initialvalue = [];
+    
     const newCustomers = customers.reduce((acumulado, customer) => {
       if(customer.id === id) {
           return [...acumulado, customerPayload];
